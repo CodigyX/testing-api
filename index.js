0c2412840432c3c5585dee5e08575504944a2bee
CodigyX <erickbg8919@gmail.com>
2023-10-10 15:47:35 -0600

    Create repository testing-api

---
+++ index.js
@@ -0,0 +1,26 @@
+const cors = require('cors')
+const express = require('express')
+
+
+const app = express();
+app.use(cors());
+app.options('*', cors());
+
+const port = 8080;
+
+app.get('/', (req, res, next) => {
+	res.send('node express api <br> by bobyDev');
+});
+
+
+app.get('/hours', (req, res, next) => {
+  res.sendfile("assets/1a_dev_hours.json");
+});
+
+app.get('/size', (req, res, next) => {
+	res.sendfile("assets/1a_proxy_size.json");
+  });
+
+app.listen(port,  () => 
+	console.log('listening on port ' + port
+));
