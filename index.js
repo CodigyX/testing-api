0e6c30860431610e63e37ba3aa1d5eb1e540fb99
CodigyX <erickbg8919@gmail.com>
2023-10-13 00:28:33 -0600

    Update issue #2

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ app.get('/', (req, res, next) => {
 	res.send('node express api <br> by CodigyX');
 });
 
-
-app.get('/hours', (req, res, next) => {
-  res.sendfile("assets/1a_dev_hours.json");
-});
-
 app.get('/size', (req, res, next) => {
 	res.sendfile("assets/1a_proxy_size.json");
+  });
+
+  app.get('/hours', (req, res, next) => {
+	res.sendfile("assets/1a_dev_hours.json");
   });
 
 app.listen(port,  () => 
