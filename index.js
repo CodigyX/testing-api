3049ac31f1701b46b6b498534ccf376263573d37
CodigyX <erickbg8919@gmail.com>
2023-10-22 17:18:50 -0600

    add issue 3a test4 #6

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.get('/3a3', (req, res, next) => {
 	res.sendfile("assets/3a_test3.json");
 });
 
+app.get('/3a4', (req, res, next) => {
+	res.sendfile("assets/3a_test4.json");
+});
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
